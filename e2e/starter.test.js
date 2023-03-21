320327a867fc616d5221197698f20ed4c84374cd
usamabutt101 <Usama.Asghar@101digital.io>
2023-03-21 18:45:26 +0500

    detox for alert

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -6,8 +6,16 @@ describe('Example', () => {
 
   it('show Login', async () => {
     await expect(element(by.id('title'))).toBeVisible();
+    await expect(element(by.id('nametext'))).toBeVisible();
+    await element(by.id('username')).tap();
     await element(by.id('username')).typeText('umair@gmail.com');
+    await expect(element(by.id('passwordtext'))).toBeVisible();
+    await element(by.id('password')).tap();
     await element(by.id('password')).typeText('123456');
     await element(by.id('button')).tap();
+    await waitFor(element(by.text('Login successful with')))
+      .toBeVisible()
+      .withTimeout(5000);
+    await element(by.text('OK')).tap();
   });
 });
